b30bb4c4ecc15d11f2f0050e3fbe8c41e6d5e458
Kehinde John <kehindejohn018@gmail.com>
2022-07-26 19:29:12 +0100

    fixed bug on sass

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -59,6 +59,7 @@
 
 <script>
 import availableParts from '../data/parts';
+import createdHookMixin from './created-hook-mixin';
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -83,6 +84,7 @@ export default {
       selectedBaseIndex: 0,
     };
   },
+  mixins: [createdHookMixin],
   computed: {
     saleBorderClass() {
       return this.selectedRobot.head.onSale ? 'sale-border' : '';
@@ -178,15 +180,17 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .part {
   position: relative;
   width:165px;
   height:165px;
   border: 3px solid #aaa;
 }
-.part img {
-  width:165px;
+.part {
+  img {
+  width: 165px;
+  }
 }
 .top-row {
   display:flex;
